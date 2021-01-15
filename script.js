53ad7b152c0415ac321427db6357ed0b833cedb4
Athena Hernandez <athena.hernandez.23@Bshops.com>
2021-01-15 07:56:32 -0800

    JSON practice :sweat_smile:

--- script.js
+++ script.js
@@ -3,34 +3,35 @@ myDiv = document.getElementById("myDiv");
 console.log(myDiv);
 
 // Click event to attach to button
-function myClick () {
-    var name = document.getElementById('name').value;
-    localStorage.setItem("name", name);
-    var date = document.getElementById('month').value;
-    var email = document.getElementById('email').value;
-    var activity = document.getElementById('yes').checked;
+function myClick() {
     var boxes = document.getElementsByName('mitf');
-    var levels = ["None", "Pre-Preliminary", "Preliminary", "Pre-Juvenile", "Juvenile", "Intermediate", "Novice", "Junior", "Senior"];
-    // Without storing these values early on, they are lost
-    var levelsPassed = [boxes[0].checked, boxes[1].checked, boxes[2].checked, boxes[3].checked, boxes[4].checked, boxes[5].checked, boxes[6].checked, boxes[7].checked, boxes[8].checked];
+    var data = {
+        'name' : document.getElementById('name').value,
+        'date' : document.getElementById('month').value,
+        'email' : document.getElementById('email').value,
+        'activity' : document.getElementById('yes').checked,
+        'levels' : ["None", "Pre-Preliminary", "Preliminary", "Pre-Juvenile", "Juvenile", "Intermediate", "Novice", "Junior", "Senior"],
+        'levelsPassed' : [boxes[0].checked, boxes[1].checked, boxes[2].checked, boxes[3].checked, boxes[4].checked, boxes[5].checked, boxes[6].checked, boxes[7].checked, boxes[8].checked],
+    };
+    localStorage.setItem("name", data['name']);
 
     // Replace content
     myDiv.innerHTML = "\n";
     
-    if (activity) {
-        myDiv.innerHTML += "\t\t<h1>" + name + " likes to figure skate &lt;3</h1>\n";
+    if (data['activity']) {
+        myDiv.innerHTML += "\t\t<h1>" + data['name'] + " likes to figure skate &lt;3</h1>\n";
     } else {
-        myDiv.innerHTML += "\t\t<h1>" + name + " <i>doesn't</i> (!!) like to figure skate >:(</h1>\n";
+        myDiv.innerHTML += "\t\t<h1>" + data['name'] + " <i>doesn't</i> (!!) like to figure skate >:(</h1>\n";
     }
 
     // Lists tests passed
-    myDiv.innerHTML += "\t\t<h3>You've passed (as of " + date + "):</h3>\n";
-    for (var i = 0; i < levels.length; i++) {
-        if (levelsPassed[i]) {
-            myDiv.innerHTML += "\t\t<ul>~ " + levels[i] + " :]</ul>\n";
+    myDiv.innerHTML += "\t\t<h3>You've passed (as of " + data['date'] + "):</h3>\n";
+    for (var i = 0; i < data['levels'].length; i++) {
+        if (data['levelsPassed'][i]) {
+            myDiv.innerHTML += "\t\t<ul>~ " + data['levels'][i] + " :]</ul>\n";
         }
     }
     
     myDiv.innerHTML += "\t\t<p>Local storage saved your name: " + localStorage.getItem("name") + "\n</p>";
-    myDiv.innerHTML += "\t\t<p>Dis u? --> " + email + "<\p>";
+    myDiv.innerHTML += "\t\t<p>Dis u? --> " + data['email'] + "<\p>";
 }
